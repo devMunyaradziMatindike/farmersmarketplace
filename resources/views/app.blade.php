88cef96678978accfc8b046a94c492948f497b14
Munyaradzi Matindike <munyaradzimatindike@Munyaradzis-MacBook-Air.local>
2025-10-30 11:46:28 +0200

    chore(favicon): replace Laravel default icons with Musika Wedu brand icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -53,10 +53,8 @@
         <link rel="canonical" href="https://musikawedu.co.zw">
         
         <!-- Favicon -->
-        <link rel="icon" type="image/x-icon" href="/favicon.ico">
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="icon" type="image/png" href="/images/musika-wedu-grain-logo.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/musika-wedu-grain-logo.png">
         <link rel="manifest" href="/site.webmanifest">
         
         <!-- Preconnect for Performance -->
